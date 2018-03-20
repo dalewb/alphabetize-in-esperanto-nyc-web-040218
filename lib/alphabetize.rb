05249d7a57e055b5502229416a92076596d45061
William Dale <williambdale@gmail.com>
2018-03-20 20:41:35 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -4,10 +4,18 @@ def alphabetize(arr)
   esp_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   
   ranking_hash = {} 
+  first_word_value = {}
   
   esp_alphabet.split('').each_with_index do |letter,idx|
     ranking_hash[letter] = idx 
   end 
   
-  binding.pry 
+  arr.each do |phrase|
+    first_word = phrase.split(' ').first 
+    order_num = ranking_hash[first_word[0]]
+    first_word_value[phrase] = order_num 
+  end 
+  
+  first_word_value.sort_by {|phrase, order_num| order_num} 
+  
 end

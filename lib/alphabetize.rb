5459c44a29819ddf2e4e7009f0f00948bfcc1eb1
William Dale <williambdale@gmail.com>
2018-03-20 21:17:53 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,10 @@
+require 'pry'
+
 def alphabetize(arr)
-  # code here
-end
+  alpha = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".chars
+
+  arr.sort_by do |phrase|
+    phrase.chars.map { |c| alpha.index(c) }
+  end
+  
+end 
